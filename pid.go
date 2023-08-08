66b6df7788276bdef265531b74f420dbad7f0414
Tom Anderson <dante.renevo@gmail.com>
2023-08-07 23:31:48 -0700

    Extracting pid into its own file

---
+++ pid.go
@@ -0,0 +1,31 @@
+package actor
+
+import "strings"
+
+type PID struct {
+	Address string
+	ID      string
+}
+
+func NewPID(address string, name string, tags ...string) PID {
+	return PID{
+		Address: address,
+		ID:      strings.Join(append([]string{name}, tags...), pidSeparator),
+	}
+}
+
+func (p PID) Equals(pid PID) bool {
+	return pid.Address == p.Address && pid.ID == p.ID
+}
+
+func (p PID) String() string {
+	return p.Address + pidSeparator + p.ID
+}
+
+func (p PID) Child(name string, tags ...string) PID {
+	return NewPID(p.Address, p.ID, append([]string{name}, tags...)...)
+}
+
+func (p PID) IsZero() bool {
+	return p.Address == "" && p.ID == ""
+}
--- processor.go
+++ processor.go
@@ -3,38 +3,9 @@ package actor
 import (
 	"fmt"
 	"os"
-	"strings"
 	"sync"
 	"time"
 )
-
-type PID struct {
-	Address string
-	ID      string
-}
-
-func NewPID(address string, name string, tags ...string) PID {
-	return PID{
-		Address: address,
-		ID:      strings.Join(append([]string{name}, tags...), pidSeparator),
-	}
-}
-
-func (p PID) Equals(pid PID) bool {
-	return pid.Address == p.Address && pid.ID == p.ID
-}
-
-func (p PID) String() string {
-	return p.Address + pidSeparator + p.ID
-}
-
-func (p PID) Child(name string, tags ...string) PID {
-	return NewPID(p.Address, p.ID, append([]string{name}, tags...)...)
-}
-
-func (p PID) IsZero() bool {
-	return p.Address == "" && p.ID == ""
-}
 
 type Processor interface {
 	PID() PID
